14e3e7bcc04c817bcb75557368576c977913f293
LAB_F_REAL <labf@continental.edu.pe>
2019-11-23 13:09:22 -0500

    cambios

--- app/src/main/java/com/example/myapplication/AuthActivity.java
+++ app/src/main/java/com/example/myapplication/AuthActivity.java
@@ -1,13 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.example.myapplication.models.Users;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -21,15 +20,17 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         btnLogin = findViewById(R.id.btnLogin);
         txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( txtUser.getText().toString().equals("alcachofa") ) {
-                    Intent o = new Intent(AuthActivity.this,
-                            MainActivity.class);
-                    startActivity(o);
-                }
+               if (Users.passwordIsValid(txtUser.getText().toString(),
+                       txtPassword.getText().toString())) {
+                   Intent o = new Intent(AuthActivity.this,
+                           MainActivity.class);
+                   startActivity(o);
+               }
             }
         });
     }
